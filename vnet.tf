7c4e6cd476ea599f10968e385ab494537e1545a3
Joe Caulfield <jcaulfield@mtb.com>
2021-06-07 16:41:10 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/joecaulfield-training/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "network"
+  resource_group_name = "my-resources"
 }
